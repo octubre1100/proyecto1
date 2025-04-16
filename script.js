f75dd90943bd39db967e224c1d1756af76be4658
octubre1100 <136340911+octubre1100@users.noreply.github.com>
2025-04-16 20:19:28 -0300

    listo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let buton = document.querySelector("#modoOscuro")
 let body = document.querySelector("body")
 buton.onclick = function () {
-body.classList.toggle("dark")
+    body.classList.toggle("dark")
 }
